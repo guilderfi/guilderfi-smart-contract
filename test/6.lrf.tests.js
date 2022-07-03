491a6cfe49424fd39dbfc08e13ab3d19b987d49a
Tymoran <tymoran@protonmail.com>
2022-07-03 12:41:00 +0200

    Fix LRF figures

--- test/6.lrf.tests.js
+++ test/6.lrf.tests.js
@@ -16,13 +16,13 @@ const account1 = createWallet(ethers);
 const account2 = createWallet(ethers);
 const account3 = createWallet(ethers);
 
-describe(`Testing liquidity relief fund..`, function () {
+describe.only(`Testing liquidity relief fund..`, function () {
   before(async function () {
     // Set up accounts
     [deployer, treasury] = await ethers.getSigners();
 
     print(`Deploying smart contracts..`);
-    token = await deploy({ ethers, deployer, treasury });
+    token = await deploy({ ethers, deployer, treasury: account1 });
 
     // contracts
     router = await ethers.getContractAt("IDexRouter", await token.getRouter());
@@ -30,19 +30,19 @@ describe(`Testing liquidity relief fund..`, function () {
     lrf = await ethers.getContractAt("LiquidityReliefFund", await token.getLrfAddress());
 
     // transfer some eth to test accounts
-    await transferEth({ from: deployer, to: account1, amount: ether(10000) });
+    await transferEth({ from: deployer, to: account1, amount: ether(5000) });
     await transferEth({ from: deployer, to: account2, amount: ether(10000) });
     await transferEth({ from: deployer, to: account3, amount: ether(10000) });
 
-    await token.connect(treasury).approve(router.address, MAX_INT);
+    await token.connect(account1).approve(router.address, MAX_INT);
 
     // Add 100k tokens + 500k BNB into liquidity
-    const tokenAmount = ether(100000);
-    const ethAmount = ether(500000);
+    const tokenAmount = ether(5000);
+    const ethAmount = ether(2500);
 
     await addLiquidity({
       router,
-      from: treasury,
+      from: account1,
       token,
       tokenAmount,
       ethAmount,
@@ -54,7 +54,7 @@ describe(`Testing liquidity relief fund..`, function () {
   });
 
   it("Should buy tokens when backed liquidity > 100%", async function () {
-    await sellTokens({ router, token, account: treasury, tokenAmount: ether(500000) });
+    await sellTokens({ router, token, account: account1, tokenAmount: ether(500) });
 
     // Ensure ratio is above MIDPOINT
     const ratioBefore = await lrf.getBackedLiquidityRatio();
@@ -62,9 +62,11 @@ describe(`Testing liquidity relief fund..`, function () {
 
     const LRFBalanceBefore = await token.balanceOf(lrf.address);
 
-    await lrf.connect(treasury).execute();
+    await lrf.connect(account1).execute();
 
     const ratioAfter = await lrf.getBackedLiquidityRatio();
+
+    console.log("ratios", ratioBefore, ratioAfter);
     // Ensure ratio is back to MIDPOINT
     expect(ratioAfter).to.equal(10000);
 
@@ -72,11 +74,11 @@ describe(`Testing liquidity relief fund..`, function () {
 
     // LRF should have increased token balance
     expect(LRFBalanceAfter.sub(LRFBalanceBefore)).to.gt(0);
-    await buyTokensFromDex({ router, pair, token, account: treasury, tokenAmount: ether(500000) });
+    await buyTokensFromDex({ router, pair, token, account: account1, tokenAmount: ether(500) });
   });
 
   it("Should sell tokens when backed liquidity < 100%", async function () {
-    await buyTokensFromDex({ router, pair, token, account: treasury, tokenAmount: ether(500000) });
+    await buyTokensFromDex({ router, pair, token, account: account1, tokenAmount: ether(500) });
 
     // Ensure ratio is below MIDPOINT
     const ratioBefore = await lrf.getBackedLiquidityRatio();
@@ -84,7 +86,7 @@ describe(`Testing liquidity relief fund..`, function () {
 
     const LRFBalanceBefore = await token.balanceOf(lrf.address);
 
-    await lrf.connect(treasury).execute();
+    await lrf.connect(account1).execute();
 
     const ratioAfter = await lrf.getBackedLiquidityRatio();
     // Ensure ratio is back to MIDPOINT
@@ -94,11 +96,11 @@ describe(`Testing liquidity relief fund..`, function () {
 
     // LRF should have decreased token balance
     expect(LRFBalanceAfter.sub(LRFBalanceBefore).lt(0));
-    await sellTokens({ router, token, account: treasury, tokenAmount: ether(500000) });
+    await sellTokens({ router, token, account: account1, tokenAmount: ether(500) });
   });
 
   it("Should not execute when backed liquidity > 115%", async function () {
-    await sellTokens({ router, token, account: treasury, tokenAmount: ether(500000) });
+    await sellTokens({ router, token, account: account1, tokenAmount: ether(500) });
 
     // Ensure ratio is above HIGH_CAP
     const ratioBefore = await lrf.getBackedLiquidityRatio();
@@ -106,7 +108,7 @@ describe(`Testing liquidity relief fund..`, function () {
 
     const LRFBalanceBefore = await token.balanceOf(lrf.address);
 
-    await lrf.connect(treasury).execute();
+    await lrf.connect(account1).execute();
 
     const ratioAfter = await lrf.getBackedLiquidityRatio();
 
@@ -117,13 +119,11 @@ describe(`Testing liquidity relief fund..`, function () {
 
     // LRF should have the same token balance
     expect(LRFBalanceAfter).to.eq(LRFBalanceBefore);
-    await buyTokensFromDex({ router, pair, token, account: treasury, tokenAmount: ether(500000) });
+    await buyTokensFromDex({ router, pair, token, account: account1, tokenAmount: ether(500) });
   });
 
   it("Should not execute when backed liquidity < 85%", async function () {
-    await buyTokensFromDex({ router, pair, token, account: treasury, tokenAmount: ether(500000) });
-    // await buyTokensFromDex({ router, pair, token, account: treasury, tokenAmount: ether(500000) });
-    console.log("LRF ratio 2", await lrf.getBackedLiquidityRatio());
+    await buyTokensFromDex({ router, pair, token, account: account1, tokenAmount: ether(500) });
 
     // Ensure ratio is below LOW_CAP
     const ratioBefore = await lrf.getBackedLiquidityRatio();
@@ -131,7 +131,7 @@ describe(`Testing liquidity relief fund..`, function () {
 
     const LRFBalanceBefore = await token.balanceOf(lrf.address);
 
-    await lrf.connect(treasury).execute();
+    await lrf.connect(account1).execute();
 
     const ratioAfter = await lrf.getBackedLiquidityRatio();
 
@@ -142,6 +142,6 @@ describe(`Testing liquidity relief fund..`, function () {
 
     // LRF should have the same token balance
     expect(LRFBalanceAfter).to.eq(LRFBalanceBefore);
-    await sellTokens({ router, token, account: treasury, tokenAmount: ether(500000) });
+    await sellTokens({ router, token, account: account1, tokenAmount: ether(500) });
   });
 });
